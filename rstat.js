6b357d2c41134cd487eaec65033f26ff3062389f
Claxton <daniel.j.claxton@gmail.com>
2018-07-08 16:13:07 -0500

    this is most definitely not right

--- rstat.js
+++ rstat.js
@@ -1,30 +1,43 @@
 module.exports = function(RED) {
     'use strict';
     var rtstat = require('rtstat');
-    var tstat = rtstat.tstat('192.168.1.6');
+    //var tstat = rtstat.tstat('192.168.1.6');
 
 
-    function RadioTherm(n) {
-        RED.nodes.createNode(this, n);
+    function RadioThermIn(n) {
 
         var node = this;
-        //this.config = n.config;
-        //this.myConfig = RED.nodes.getNode(this.config);
+
+        RED.nodes.createNode(node, n);
+
+        var tlist = rtstat.findThermostats();
+
+        // which returns a promise that will resolve to an object with tstat uuids as keys and tstat objects as, um, objects
+
+        tlist.then(function(thermostats) {
+            for (var key in thermostats) {
+                // 'key' is this thermostat's uuid
+                var thisTstat = thermostats[key];
+            }
+        });
+
+
+        //if (node.myConfig) {
 
         // initialize object
         //var obj = new rstat(this, config)
 
         // on input message
-        this.on('input', function(msg) {
+        node.on('input', function(msg) {
             //obj.handleInputEvent(msg)
-            var outputPromise = tstat.ttemp()
-                .then(function(value) {
-                    node.send({ payload: value });
-                });
+            //var outputPromise = tstat.ttemp();
+            //outputPromise.then(function(value) {
+            //    node.send({ payload: value });
+            //});
         });
 
     }
-    RED.nodes.registerType("rstat", RadioTherm);
+    RED.nodes.registerType("rstat in", RadioThermIn);
 
 
 }
